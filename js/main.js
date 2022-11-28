1227b3592d8fad245733e17c1c1e9dd1832bbabe
pel1780 <118698633+pel1780@users.noreply.github.com>
2022-11-28 17:48:10 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    // $('.mainSlide').slick();
+    $('.mainSlide').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+})
