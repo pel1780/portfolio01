6fe8a01a72eba62ff5b217c0ce5ac68f856ef43e
pel1780 <118698633+pel1780@users.noreply.github.com>
2022-11-29 17:52:24 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
 $(function () {
     // $('.mainSlide').slick();
     $('.mainSlide').slick({
+        dots: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 3000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+
+    $('.product_slide').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 })
